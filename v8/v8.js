ea0d8905420b5eee710b8d179417c38e2adf34ec
vallzter <valgeiringi@gmail.com>
2017-10-19 12:15:38 +0000

    Add files via upload

---
+++ v8/v8.js
@@ -0,0 +1,18 @@
+/*
+Verkefni 8 
+
+Valgeir Ingi
+19.10.2017
+
+1.Hver er munurinn á for, forEach, for-in og for-of lykkjum?
+	for loop--> Test statementið mun virkjast ef tiltekið ástand er true eða ekki. 
+	Ef það er true þá mun kóðin sem er í loopuni vera framkvæmdur ef 
+	hann er ekki true þá fer hann úr loopunni.
+	-------------------------------------------
+	forEach--> Aðferðin framkvæmir tiltekna aðferð einu sinni per array 
+	element og framkvæmir callback fyrir hvern array lið og tekur einungis 
+	að sér 3 arguments sem eru value, index og array.
+	-------------------------------------------------
+	for-in--> 
+	--------------------------------------------------
+	for-of--> 
